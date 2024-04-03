4ecd15918468add7a92fca1e7c97e29bf5540e7b
adrian <uo287621@uniovi.es>
2024-04-03 02:04:31 +0200

    modified userSchema

--- users/userservice/user-model.js
+++ users/userservice/user-model.js
@@ -18,12 +18,114 @@ const userSchema = new mongoose.Schema({
       type: Date,
       default: Date.now, 
     },
-    points: {
-      type: Number,
-      default: function() {
-        // Generate a random integer between 0 and 100
-        return Math.floor(Math.random() * 101);
-      }
+    ranking: {
+      total_points: {
+        type: Number,
+        default: 0,
+      },
+      total_questions: {
+        type: Number,
+        default: 0,
+      },
+      total_correct: {
+        type: Number,
+        default: 0,
+      },
+      total_wrong: {
+        type: Number,
+        default: 0,
+      },
+
+      score_flags: {
+        points_flags: {
+          type: Number,
+          default: 0,
+        },
+        questions_flags: {
+          type: Number,
+          default: 0,
+        },
+        correct_flags: {
+          type: Number,
+          default: 0,
+        },
+        wrong_flags: {
+          type: Number,
+          default: 0,
+        },
+      },
+      score_cities: {
+        points_cities: {
+          type: Number,
+          default: 0,
+        },
+        questions_cities: {
+          type: Number,
+          default: 0,
+        },
+        correct_cities: {
+          type: Number,
+          default: 0,
+        },
+        wrong_cities: {
+          type: Number,
+          default: 0,
+        },
+      },
+      score_monuments: {
+        points_monuments: {
+          type: Number,
+          default: 0,
+        },
+        questions_monuments: {
+          type: Number,
+          default: 0,
+        },
+        correct_monuments: {
+          type: Number,
+          default: 0,
+        },
+        wrong_monuments: {
+          type: Number,
+          default: 0,
+        },
+      },
+      score_tourist: {
+        points_tourist: {
+          type: Number,
+          default: 0,
+        },
+        questions_tourist: {
+          type: Number,
+          default: 0,
+        },
+        correct_tourist: {
+          type: Number,
+          default: 0,
+        },
+        wrong_tourist: {
+          type: Number,
+          default: 0,
+        },
+      },
+      score_foods: {
+        points_foods: {
+          type: Number,
+          default: 0,
+        },
+        questions_foods: {
+          type: Number,
+          default: 0,
+        },
+        correct_foods: {
+          type: Number,
+          default: 0,
+        },
+        wrong_foods: {
+          type: Number,
+          default: 0,
+        },
+      },
     }
 });
 
