dd647c7dd8acf47b2f26c83fcd18c1fa7da0cadf
uo287841 <uo287841@uniovi.es>
2024-04-08 01:02:04 +0200

    register 2e2 test

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -3,6 +3,10 @@ const { defineFeature, loadFeature }=require('jest-cucumber');
 const setDefaultOptions = require('expect-puppeteer').setDefaultOptions
 const feature = loadFeature('./features/register-form.feature');
 
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const mockAxios = new MockAdapter(axios);
+
 let page;
 let browser;
 
@@ -11,7 +15,7 @@ defineFeature(feature, test => {
   beforeAll(async () => {
     browser = process.env.GITHUB_ACTIONS
       ? await puppeteer.launch()
-      : await puppeteer.launch({ headless: false, slowMo: 100 });
+      : await puppeteer.launch({ headless: false, slowMo:5 });
     page = await browser.newPage();
     //Way of setting up the timeout
     setDefaultOptions({ timeout: 10000 })
@@ -21,27 +25,42 @@ defineFeature(feature, test => {
         waitUntil: "networkidle0",
       })
       .catch(() => {});
+
+      
+  });
+
+  beforeEach(async () => {
+    // Reset any state or actions before each test
+    await page.reload({ waitUntil: 'networkidle0' });
   });
 
   test('The user is not registered in the site', ({given,when,then}) => {
     
     let username;
     let password;
+    let email
 
     given('An unregistered user', async () => {
-      username = "pablo"
-      password = "pabloasw"
-      await expect(page).toClick("button", { text: "Don't have an account? Register here." });
+      username = "t1"
+      email = "t1email"
+      password = "t1pass"
+
+      await expect(page).toClick("button", { text: "Create account" });
     });
 
     when('I fill the data in the form and press submit', async () => {
       await expect(page).toFill('input[name="username"]', username);
+      await expect(page).toFill('input[name="email"]', email);
       await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toClick('button', { text: 'Add User' })
+      await expect(page).toFill('input[name="cpassword"]', password);      
+
+      mockAxios.onPost('http://localhost:8000/adduser').reply(200, { username:  "t1", email: "t1email", password: "t1pass" });
+
+      await expect(page).toClick('button', { text: 'Register' })
     });
 
-    then('A confirmation message should be shown in the screen', async () => {
-        await expect(page).toMatchElement("div", { text: "User added successfully" });
+    then('is taken to login', async () => {
+        await expect(page).toMatchElement("div", { text: "Login" });
     });
   })
 
