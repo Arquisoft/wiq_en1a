3f5f6d4a7b65f22eba16a0caf0047cd23ad89e72
Andrés Cadenas <77900120+andrrsin@users.noreply.github.com>
2024-04-21 20:30:07 +0200

    Game Report

--- webapp/src/components/Question.jsx
+++ webapp/src/components/Question.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
+import ImgGameReport from "./ImgGameReport";
 
 const Question = (props) => {
     const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
@@ -14,23 +15,33 @@ const Question = (props) => {
     const questionsPerGame = 10;
     const imagesPerQuestion = 4;
 
+    //Game Report
+    const [finished, setFinished] = useState(false);
+    const [answers, setAnswers] = useState([]);
+
+
+
     useEffect(() => {
         fetchQuestions();
     }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if(renderedImages==imagesPerQuestion){
+            if (renderedImages == imagesPerQuestion) {
                 setCounter((prevCounter) => prevCounter + 0.4);
-            } 
+            }
         }, 40);
 
         return () => clearInterval(interval);
     }, [renderedImages]);
 
     useEffect(() => {
+        async function answ() {
+            await answerQuestion("TimeOut1234;", questions[currentQuestion].question);
+        }
+
         if (counter >= 100 && !loading) {
-            answerQuestion("",questions[currentQuestion]);
+            answ();
         }
     }, [counter]);
 
@@ -39,7 +50,7 @@ const Question = (props) => {
         try {
             setRenderedImages(0)
             let auxQuestions = []
-            for(let i=0;i<questionsPerGame;i++){
+            for (let i = 0; i < questionsPerGame; i++) {
                 let question = ((await axios.get(`${apiEndpoint}/${props.type}/${props.category}/question`)).data)
                 auxQuestions.push(question)
             }
@@ -54,31 +65,50 @@ const Question = (props) => {
         try {
             setLoading(true);
             setRenderedImages(0)
-            if(currentQuestion>=questionsPerGame-1){
-                fetchQuestions()
-                setCurrentQuestion(0)
-                setCounter(0);
+            
+
+            const result = await axios.post(`${apiEndpoint}/${props.type}/answer`,
+                { answer: answer, question: question, username: auth.username, category: props.category },
+                { headers: { 'Content-Type': 'application/json' } });
+            
+            if (result.data.correct === "true") {
+                setScore(score + 1);
+                setAnswers(answers.concat( {question:question,correct:result.data.correct,associate:answer}));
+            }else{
+                setAnswers(answers.concat( {question:question,correct:result.data.correct,associate:result.data.correctImg}));
+            }
+            
+            if (currentQuestion >= questionsPerGame - 1) {
+                //Infinte Game
+                // fetchQuestions()
+                // setCurrentQuestion(0)
+                // setCounter(0);
+                setFinished(true);
+                setLoading(false);
                 return
             }
-
-            const result = await axios.post(`${apiEndpoint}/${props.type}/answer`, 
-                { answer: answer, question:question, username: auth.username, category: props.category },
-                { headers: {'Content-Type': 'application/json'}});
-
-            if ( result.data.correct === "true" ) {
-                setScore(score +1);
-            }
-            setCurrentQuestion((question)=>question+1)
+            setCurrentQuestion((question) => question + 1)
             setCounter(0);
             setLoading(false)
         } catch (error) {
-            console.log(error);
+            //console.log(error);
         }
     }
 
-    
+    const restartGame = async () => {
+        setLoading(true);
+        setScore(0);
+        setAnswers([]);
+        setCurrentQuestion(0);
+        setCounter(0);
+        setFinished(false);
+        await fetchQuestions();
+        setLoading(false);
+    }
+
 
     return (
+        finished ? <ImgGameReport score={score} answers={answers} restartGame={restartGame} /> :
         <div className="bg-slate-50 shadow-lg rounded-md p-4 mx-auto max-w-2xl mt-16">
             <div className="absolute bottom-0 left-0 p-2 bg-gray-200 rounded-md">
                 Score: {score}
@@ -89,7 +119,7 @@ const Question = (props) => {
                         <div class="rounded-full h-20 w-20 bg-violet-800 animate-ping"></div>
                     </div></h1>
                 </>
-            ) : (
+            ) :  (
                 <>
                     <h1 className="font-bold text-3xl text-gray-800 pl-8">{questions[currentQuestion].question}</h1>
                     <div class="relative h-5 rounded-full overflow-hidden bg-gray-300 mt-20 mx-10">
@@ -100,8 +130,8 @@ const Question = (props) => {
                         {questions[currentQuestion].images.map(image => (
                             <button className="transition-transform transform-gpu hover:scale-105 rounded-xl mx-8 my-8 max-h-52 max-w-80">
                                 <img src={image} alt='Loading image...' className="rounded-lg object-contain shadow-md"
-                                    onClick={() => answerQuestion(image,questions[currentQuestion].question)}
-                                    onLoad={()=> setRenderedImages(renderedImages=> renderedImages+1)}></img>
+                                    onClick={async () => await answerQuestion(image, questions[currentQuestion].question)}
+                                    onLoad={() => setRenderedImages(renderedImages => renderedImages + 1)}></img>
                             </button>
                         ))}
                     </div>
